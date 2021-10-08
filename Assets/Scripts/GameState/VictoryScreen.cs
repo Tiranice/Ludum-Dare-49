f3365d07b646222121c89e9c4e4ff4d780fe1874
Tiranice <35855236+Tiranice@users.noreply.github.com>
2021-10-08 00:11:41 -0500

    Victory screen pauses the game.

--- Assets/Scripts/GameState/VictoryScreen.cs
+++ Assets/Scripts/GameState/VictoryScreen.cs
@@ -1,6 +1,8 @@
+using TirUtilities.Experimental;
 using TirUtilities.Extensions;
 using TirUtilities.Signals;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace LudumDare49.GameStateControl
 {
@@ -12,7 +14,7 @@ namespace LudumDare49.GameStateControl
     /// Author :  Devon Wilson
     /// Company:  Black Pheonix Software
     /// Created:  Oct 03, 2021
-    /// Updated:  Oct 04, 2021
+    /// Updated:  Oct 07, 2021
     /// -->
     /// <summary>
     ///
@@ -23,17 +25,28 @@ namespace LudumDare49.GameStateControl
 
         [SerializeField] private RectTransform _panel;
 
+        private ApplicationStateMachine _applicationStateMachine;
+
         #endregion
 
         #region Events & Signals
 
+        [Header("Signals")]
         [SerializeField] private Signal _victorySignal;
+
+        [Header("Events")]
+        public UnityEvent OnOpened;
+        public UnityEvent OnClosed;
 
         #endregion
 
         #region Unity Messages
 
-        private void Awake() => AssignReceivers();
+        private void Awake()
+        {
+            _applicationStateMachine = FindObjectOfType<ApplicationStateMachine>();
+            AssignReceivers();
+        }
 
         private void OnDestroy() => RemoveReceivers();
 
@@ -45,22 +58,31 @@ namespace LudumDare49.GameStateControl
         {
             if (_victorySignal.NotNull())
                 _victorySignal.AddReceiver(Open);
+
+            OnOpened.AddListener(_applicationStateMachine.BlockPause);
+            OnClosed.AddListener(_applicationStateMachine.AllowPause);
         }
 
         private void RemoveReceivers()
         {
             if (_victorySignal.NotNull())
                 _victorySignal.RemoveReceiver(Open);
+
+            OnOpened.RemoveListener(_applicationStateMachine.BlockPause);
+            OnClosed.RemoveListener(_applicationStateMachine.AllowPause);
         }
 
         #endregion
 
         #region Public Methods
 
-        public void Open() => _panel.gameObject.SetActive(true);
-        public void Close() => _panel.gameObject.SetActive(false);
-        public void Toggle() => _panel.gameObject.SetActive(!_panel.gameObject.activeSelf);
-        public void SetOpen(bool state) => _panel.gameObject.SetActive(state);
+        public void Open() { _panel.gameObject.SetActive(true); OnOpened.SafeInvoke(); }
+
+        public void Close() { _panel.gameObject.SetActive(false); OnClosed.SafeInvoke(); }
+
+        public void Toggle() { if (_panel.gameObject.activeSelf) Close(); else Open(); }
+
+        public void SetOpen(bool state) { if (state) Open(); else Close(); }
 
         #endregion
 
