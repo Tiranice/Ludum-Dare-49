c00cd9ede853826c9fcef5a2574fd17fada615f8
Tiranice <35855236+Tiranice@users.noreply.github.com>
2021-10-08 00:48:54 -0500

    Corrects the signal receiving in ActiveSelfSetter

--- Assets/Scripts/ActiveSelfSetter.cs
+++ Assets/Scripts/ActiveSelfSetter.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TirUtilities.Extensions;
 using TirUtilities.Signals;
 using UnityEngine;
@@ -15,32 +13,60 @@ namespace LudumDare49
     /// Author :  Devon Wilson
     /// Company:  Black Pheonix Software
     /// Created:  Oct 03, 2021
-    /// Updated:  Oct 03, 2021
+    /// Updated:  Oct 07, 2021
     /// -->
     /// <summary>
     ///
     /// </summary>
     public class ActiveSelfSetter : MonoBehaviour
     {
+        #region Inspector Fields
+
         [SerializeField] private bool _defaultState;
         [SerializeField] private BoolSignal _lockSignal;
 
         [SerializeField] private bool _isLocked = false;
 
+        #endregion
+
+        #region Private Fields
+
         private bool _isDead = false;
 
+        #endregion
+
+        #region Events & Signals
+
         public UnityEvent OnDisabled;
+
+        #endregion
+
+        #region Unity Messages
 
         private void Awake()
         {
             if (_lockSignal.NotNull())
-                _lockSignal.AddReceiver(val => 
-                { 
-                    _isLocked = val;
-                    ToggleOn();
-                });
+                _lockSignal.AddReceiver(LockReceiver);
             gameObject.SetActive(_defaultState);
         }
+
+        private void OnDestroy()
+        {
+            if (_lockSignal.NotNull())
+                _lockSignal.RemoveReceiver(LockReceiver);
+        }
+
+        #endregion
+
+        #region Receivers
+
+        private void LockReceiver(bool val)
+        {
+            _isLocked = val;
+            ToggleOn();
+        }
+
+        #endregion
 
         public void ToggleOff()
         {
